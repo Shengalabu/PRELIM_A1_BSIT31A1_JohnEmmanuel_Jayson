cd551238aa961677bf119be3d732057719a7339c
Shengalabu <shengalabu@gmail.com>
2025-08-01 12:57:09 +0800

    Added Student Class

---
+++ IT Elective 2 - 1.1/Models/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace IT_Elective_2___11.Models
+{
+    // ✅ Gender enum definition
+    public enum Gender
+    {
+        Unknown,
+        Male,
+        Female
+    }
+
+    public class Student
+    {
+        // 🔒 Private fields
+        private string firstName;
+        private string lastName;
+        private Gender gender = Gender.Unknown;
+
+        // 📦 Public properties
+        public string Title { get; set; }
+        public string Course { get; set; }
+        public string Section { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string FirstName => firstName;
+        public string LastName => lastName;
+        public Gender Gender => gender;
+
+        public string FullName => $"{Title} {FirstName} {LastName}";
+        public int Age
+        {
+            get
+            {
+                var today = DateTime.Today;
+                var age = today.Year - Birthday.Year;
+                if (Birthday > today.AddYears(-age)) age--;
+                return age;
+            }
+        }
+
+        public Student()
+        {
+            firstName = "Default";
+            lastName = "Student";
+            Title = "Mr./Ms.";
+            Course = "Unknown";
+            Section = "Unknown";
+            Birthday = DateTime.Today;
+        }
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            Title = "Mr./Ms.";
+            Course = "Unknown";
+            Section = "Unknown";
+            Birthday = DateTime.Today;
+        }
+
+        // 🧪 Setters
+        public void SetFirstName(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public void SetLastName(string lastName)
+        {
+            this.lastName = lastName;
+        }
+
+        public void SetGender(Gender gender)
+        {
+            this.gender = gender;
+        }
+    }
+}
